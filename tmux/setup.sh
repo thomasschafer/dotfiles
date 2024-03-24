d091eeb8b5bf9d22584bf7c80f654afb0a911f5e
thomasschafer <thomasschafer97@gmail.com>
2024-03-24 15:53:13 +0000

    Add tmux toggle pane keybinding

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -8,4 +8,6 @@ ln -sfn $(pwd)/.tmux.conf $HOME/.tmux.conf
 rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+~/.tmux/plugins/tpm/bin/install_plugins
+
 echo "Tmux configured successfully"
