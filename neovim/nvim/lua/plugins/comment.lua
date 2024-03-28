e8f7e4ad6dd7686c320afa41c78abf2ed47a00e3
thomasschafer <thomasschafer97@gmail.com>
2024-03-28 15:34:29 +0000

    Add text-case plugin and misc updates

---
+++ neovim/nvim/lua/plugins/comment.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "echasnovski/mini.comment",
+    opts = {
+      mappings = {
+        -- Toggle comment on current line
+        comment_line = "<C-v>",
+        -- Toggle comment on visual selection
+        comment_visual = "<C-v>",
+      },
+    },
+  },
+}
--- neovim/nvim/lua/plugins/telescope.lua
+++ neovim/nvim/lua/plugins/telescope.lua
@@ -6,12 +6,19 @@ return {
 
       require("telescope").setup({
         defaults = {
+          layout_strategy = "flex",
+          sorting_strategy = "ascending",
+          layout_config = {
+            prompt_position = "top",
+          },
           mappings = {
             i = {
               ["<C-q>"] = actions.send_to_qflist,
+              -- ["<C-a>"] = actions.to_fuzzy_refine,
             },
             n = {
               ["<C-q>"] = actions.send_to_qflist,
+              -- ["<C-a>"] = actions.to_fuzzy_refine,
             },
           },
         },
