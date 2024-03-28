977fd53f215109d71042114e2d18e167bb79ddc4
thomasschafer <thomasschafer97@gmail.com>
2024-03-28 23:02:53 +0000

    Add telescope-live-grep-args

--- neovim/nvim/lua/plugins/telescope.lua
+++ neovim/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,17 @@
 return {
   {
     "telescope.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope-live-grep-args.nvim" },
+    },
     config = function()
       local actions = require("telescope.actions")
+      require("telescope").load_extension("live_grep_args")
+      local lga_actions = require("telescope-live-grep-args.actions")
+
+      vim.keymap.set("n", "<leader>/", function()
+        require("telescope").extensions.live_grep_args.live_grep_args()
+      end, { desc = "Find files (live grep)" })
 
       require("telescope").setup({
         defaults = {
@@ -22,6 +31,17 @@ return {
             },
           },
         },
+        -- extensions = {
+        --   live_grep_args = {
+        --     auto_quoting = true, -- enable/disable auto-quoting
+        --     mappings = {
+        --       i = {
+        --         ["<C-u>"] = lga_actions.quote_prompt(),
+        --         -- ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+        --       },
+        --     },
+        --   },
+        -- },
       })
     end,
   },
