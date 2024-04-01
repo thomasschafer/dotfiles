3bb05e066ae6fa0ecd0d7ed241ec88915daa5d17
thomasschafer <thomasschafer97@gmail.com>
2024-04-01 16:35:39 +0100

    Add <leader>d keymap to yank when deleting

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -1,6 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 
 -- Don't save deleted lines to clipboard
 vim.keymap.set({ "n", "v" }, "d", '"_d', { noremap = true })
@@ -8,10 +7,10 @@ vim.keymap.set({ "n", "v" }, "D", '"_D', { noremap = true })
 vim.keymap.set({ "n", "v" }, "c", '"_c', { noremap = true })
 vim.keymap.set({ "n", "v" }, "C", '"_C', { noremap = true })
 vim.keymap.set({ "n", "v" }, "S", '"_S', { noremap = true })
-vim.keymap.set("n", "x", '"_x', { noremap = true })
-vim.keymap.set("v", "D", '"_D', { noremap = true })
 vim.keymap.set("v", "p", '"_dP', { noremap = true })
 vim.keymap.set("v", "P", '"_dP', { noremap = true })
+-- Yank to system clipboard when deleting with leader prefix
+vim.keymap.set({ "n", "v" }, "<leader>d", '"+d', { noremap = true })
 
 vim.keymap.set({ "n", "i" }, "<F4>", ":cnext<CR>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "i" }, "<F16>", ":cprev<CR>", { noremap = true, silent = true }) -- <S-F4>
