1a800f38dbc875b0ea86813a3749372514121991
thomasschafer <thomasschafer97@gmail.com>
2024-04-03 09:26:55 +0100

    Use built-in codeium extra

--- neovim/nvim/lua/config/lazy.lua
+++ neovim/nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.editor.trouble-v3" },
     { import = "lazyvim.plugins.extras.editor.mini-move" },
+    { import = "lazyvim.plugins.extras.coding.codeium" },
     -- import/override with your plugins
     { import = "plugins" },
   },
