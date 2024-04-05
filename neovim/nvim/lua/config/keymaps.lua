5d3dc3d779c008c7426c3170ce141d41c073b0e8
thomasschafer <thomasschafer97@gmail.com>
2024-04-05 10:27:30 +0100

    Update plugins and add yanking keymap

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -3,14 +3,14 @@
 
 -- Don't save deleted lines to clipboard
 vim.keymap.set({ "n", "v" }, "d", '"_d', { noremap = true })
+vim.keymap.set({ "n", "v" }, "<leader>d", '"+d', { noremap = true })
 vim.keymap.set({ "n", "v" }, "D", '"_D', { noremap = true })
 vim.keymap.set({ "n", "v" }, "c", '"_c', { noremap = true })
 vim.keymap.set({ "n", "v" }, "C", '"_C', { noremap = true })
 vim.keymap.set({ "n", "v" }, "S", '"_S', { noremap = true })
+vim.keymap.set("n", "x", '"_x', { noremap = true })
 vim.keymap.set("v", "p", '"_dP', { noremap = true })
 vim.keymap.set("v", "P", '"_dP', { noremap = true })
--- Yank to system clipboard when deleting with leader prefix
-vim.keymap.set({ "n", "v" }, "<leader>d", '"+d', { noremap = true })
 
 vim.keymap.set({ "n", "i" }, "<F4>", ":cnext<CR>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "i" }, "<F16>", ":cprev<CR>", { noremap = true, silent = true }) -- <S-F4>
