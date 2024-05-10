dff77959c404a3b1c0f3e19ce33d7f50e64fbeb7
thomasschafer <thomasschafer97@gmail.com>
2024-05-10 20:59:06 +0100

    Add keymap to jump to parent node

--- neovim/nvim/lua/plugins/neotree.lua
+++ neovim/nvim/lua/plugins/neotree.lua
@@ -16,6 +16,14 @@ return {
           },
         },
       },
+      window = {
+        mappings = {
+          ["b"] = function(state)
+            local node = state.tree:get_node()
+            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+          end,
+        },
+      },
     },
   },
 }
