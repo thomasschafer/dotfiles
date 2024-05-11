eeeb748a4d2904ae2a0e904bdea4b8227825abbc
thomasschafer <thomasschafer97@gmail.com>
2024-05-11 10:39:19 +0100

    Update theme and misc Helix config updates

--- helix/setup.sh
+++ helix/setup.sh
@@ -4,5 +4,7 @@ set -e
 set -o pipefail
 
 ln -sfn $(pwd)/config.toml $HOME/.config/helix/config.toml
+ln -sfn $(pwd)/themes $HOME/.config/helix/themes
+ln -sfn $(pwd)/languages.toml $HOME/.config/helix/languages.toml
 
 echo "Helix configured successfully"
