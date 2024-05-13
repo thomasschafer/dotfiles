7c149deb23c54d195e743b91e8a0472fa39a0571
thomasschafer <thomasschafer97@gmail.com>
2024-05-13 12:10:32 +0100

    Remove indent animation and misc updates

--- helix/setup.sh
+++ helix/setup.sh
@@ -6,5 +6,6 @@ set -o pipefail
 ln -sfn $(pwd)/config.toml $HOME/.config/helix/config.toml
 ln -sfn $(pwd)/themes $HOME/.config/helix/themes
 ln -sfn $(pwd)/languages.toml $HOME/.config/helix/languages.toml
+ln -sfn $(pwd)/ignore $HOME/.config/helix/ignore
 
 echo "Helix configured successfully"
