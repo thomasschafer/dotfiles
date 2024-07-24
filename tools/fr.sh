367396add64f7e9bd1754a9cf2c10497e93f1c63
thomasschafer <thomasschafer97@gmail.com>
2024-07-24 22:27:40 +0100

    Add support to fr for preview and fixed-strings

--- tools/fr.sh
+++ tools/fr.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    SCRIPT_BASENAME="$(basename "$0")"
-    echo "Usage: $SCRIPT_BASENAME <BEFORE> <AFTER>"
-    exit 1
+USAGE="Usage: $(basename "$0") [-F|--fixed-strings] [-p|--preview] <BEFORE> [AFTER]"
+
+SHARED_FLAGS=""
+PREVIEW=false
+
+while [[ "$1" =~ ^- ]]; do
+    case $1 in
+        -F|--fixed-strings )
+            SHARED_FLAGS="-F"
+            shift
+            ;;
+        -p|--preview )
+            PREVIEW=true
+            shift
+            ;;
+        -* )
+            echo "$USAGE"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$PREVIEW" = true ]; then
+    if [ "$#" -ne 1 ]; then
+        echo "$USAGE"
+        exit 1
+    fi
+
+    BEFORE=$1
+
+    rg $SHARED_FLAGS "$BEFORE"
+else
+    if [ "$#" -ne 2 ]; then
+        echo "$USAGE"
+        exit 1
+    fi
+
+    BEFORE=$1
+    AFTER=$2
+
+    rg $SHARED_FLAGS -l "$BEFORE" | xargs -I {} sd $SHARED_FLAGS "$BEFORE" "$AFTER" {}
 fi
 
-BEFORE=$1
-AFTER=$2
-
-rg -l "$BEFORE" | xargs -I {} sd "$BEFORE" "$AFTER" {}
