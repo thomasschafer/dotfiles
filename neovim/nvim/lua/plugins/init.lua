bfb6441c8fce6fc6fc723e7e98e1a2c1d5d0c090
thomasschafer <thomasschafer97@gmail.com>
2024-12-10 10:04:25 +0000

    Recreate Nvim config

---
+++ neovim/nvim/lua/plugins/init.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      -- Default toggleterm config (optional)
+      size = 20,
+      open_mapping = [[<c-\>]],
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      persist_size = true,
+      direction = "float",
+    }
+  },
+})
