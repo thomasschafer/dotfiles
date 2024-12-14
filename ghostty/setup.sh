00c0522858c69cae06038ae389db39e022e0ca25
thomasschafer <thomasschafer97@gmail.com>
2024-12-14 19:23:58 +0000

    Use Ghostty template and strip end of line comments

--- ghostty/setup.sh
+++ ghostty/setup.sh
@@ -6,6 +6,9 @@ set -o pipefail
 # TODO: download Ghostty here - maybe just use brew when public
 
 mkdir -p $HOME/.config/ghostty
+
+# Strip end-of-line comments starting with ##
+sed 's/[[:space:]]*##.*$//' config.template > config
+
 ln -sfn $(PWD)/config $HOME/.config/ghostty/config
-# ~/.config/ghostty/config
 
