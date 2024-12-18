9bf32b6c686f5b10b26b06e8cba57cb5024b469f
thomasschafer <thomasschafer97@gmail.com>
2024-12-18 23:06:56 +0000

    Update Alacritty padding and icon

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -7,4 +7,6 @@ mkdir -p $HOME/.config/alacritty
 ln -sfn $(pwd)/alacritty.toml $HOME/.config/alacritty/alacritty.toml
 curl -LO --output-dir $HOME/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-macchiato.toml
 
+mv ./Alacritty.icns /Applications/Alacritty.app/Contents/Resources/alacritty.icns
+
 echo "Alacritty configured successfully"
