578970c59cea41c3eab410d2a7e9c08ae06d692b
thomasschafer <thomasschafer97@gmail.com>
2024-12-19 07:40:13 +0000

    Fix Alacritty icon script

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -7,6 +7,8 @@ mkdir -p $HOME/.config/alacritty
 ln -sfn $(pwd)/alacritty.toml $HOME/.config/alacritty/alacritty.toml
 curl -LO --output-dir $HOME/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-macchiato.toml
 
-mv ./Alacritty.icns /Applications/Alacritty.app/Contents/Resources/alacritty.icns
+cp ./alacritty.icns /Applications/Alacritty.app/Contents/Resources/alacritty.icns
+touch /Applications/Alacritty.app
+killall Finder && killall Dock
 
 echo "Alacritty configured successfully"
