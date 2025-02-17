5b7e4064cb1b105d15a7c57c64619cd4b3eb751a
Thomas Schafer <thomasschafer97@gmail.com>
2025-02-17 08:43:57 +0000

    Add Alacritty config to Nix

--- nix/modules/home.nix
+++ nix/modules/home.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   home = {
@@ -23,6 +28,23 @@
       ".config/zed/settings.json" = {
         source = ../../zed/settings.json;
       };
+
+      ".config/alacritty/catppuccin-macchiato.toml" = {
+        source = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/catppuccin/alacritty/f6cb5a5c2b404cdaceaff193b9c52317f62c62f7/catppuccin-macchiato.toml";
+          sha256 = "sha256-/Qb5kfR5N6mTMcL6l6qUdsG32wpkpESHu5qjX3GIUHw=";
+        };
+      };
+      ".config/alacritty/alacritty.toml" = {
+        source = ../../alacritty/alacritty.toml;
+      };
+    };
+
+    activation = {
+      replaceAlacrittyIcon = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        $DRY_RUN_CMD /usr/bin/sudo cp -f ${../../alacritty/alacritty.icns} /Applications/Alacritty.app/Contents/Resources/alacritty.icns
+        $DRY_RUN_CMD /usr/bin/sudo touch /Applications/Alacritty.app  # Force Finder to refresh the icon
+      '';
     };
   };
 
